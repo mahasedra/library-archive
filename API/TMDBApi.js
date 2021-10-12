8aa01b8d266bb576a0a4de85da00f1f0d6192895
mahasedra <mahasedra01@gmail.com>
2021-10-12 21:36:43 +0300

    search api tmdb film

---
+++ API/TMDBApi.js
@@ -0,0 +1,10 @@
+// API/TMDBApi.js
+
+const API_TOKEN = "9b0f85667b2a5633e724931d5738a13d";
+
+export function getFilmsFromApiWithSearchedText(text) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.error(error))
+}
