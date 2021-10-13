bb3a550665ee4b6df50caa87e9f422a4680e14cc
mahasedra <mahasedra01@gmail.com>
2021-10-13 19:42:05 +0300

    simple search and get api data

--- Components/Search.js
+++ Components/Search.js
@@ -1,7 +1,7 @@
 // Components/Search.js
 
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
@@ -11,14 +11,19 @@ class Search extends React.Component {
     super(props)
     this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
     this.state = {
-      films: []
+      films: [],
+      isLoading: false // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche
     }
   }
 
   _loadFilms() {
-    if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+    if (this.searchedText.length > 0) {
+      this.setState({ isLoading: true }) // Lancement du chargement
       getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
-          this.setState({ films: data.results })
+        this.setState({
+          films: data.results,
+          isLoading: false // Arrêt du chargement
+        })
       })
     }
   }
@@ -27,6 +32,16 @@ class Search extends React.Component {
     this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
   }
 
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+          {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+        </View>
+      )
+    }
+  }
   render() {
     console.log("RENDER")
     return (
@@ -35,13 +50,15 @@ class Search extends React.Component {
           style={styles.textinput}
           placeholder='Titre du film'
           onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._loadFilms()}
         />
-        <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+        <Button title='Rechercher' onPress={() => this._loadFilms()} />
         <FlatList
           data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
-          renderItem={({item}) => <FilmItem film={item}/>}
+          renderItem={({ item }) => <FilmItem film={item} />}
         />
+        {this._displayLoading()}
       </View>
     )
   }
@@ -59,6 +76,15 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
 
